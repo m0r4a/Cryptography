c92106a9f9eece40522a58d4e7b238940b8bd2dd
Gael Mora <m0r4a@proton.me>
2024-05-11 19:17:12 -0600

    Proyect refactored and recovered

--- AFFINE/test.py
+++ AFFINE/test.py
@@ -1,53 +1,53 @@
 from functions.letter_to_number import letter_to_number
-from functions.AFFINE_encrypt import AFFINE_encrypt
+from functions.affine_encrypt import affine_encrypt
 from functions.number_to_letter import number_to_letter
 from functions.inverse_a import inverse_a
-from functions.AFFINE_decrypt import AFFINE_decrypt
+from functions.affine_decrypt import affine_decrypt
 
 
-# Declaring variables
+# declaring variables
 
 n = 26
 block_size = 1
 a = 5
 b = 4
-N = (n ** block_size)
+n = (n ** block_size)
 
-texto_plano1 = "HIJOCOMOVAELSEMESTREZ"
-texto_plano2 = "DESEOPASARLOSEXAMENES"
-texto_plano3Z = "EXAGERATUSALEGRIASACADEMICASSIEMPREZ"
-ejemplo = "ET"
+texto_plano1 = "hijocomovaelsemestrez"
+texto_plano2 = "deseopasarlosexamenes"
+texto_plano3z = "exageratusalegriasacademicassiemprez"
+ejemplo = "et"
 
 m = ejemplo
 
-# Encryption process
+# encryption process
 
-print("------ ENCRYPTING ------\n\nText: ", m)
+print("------ encrypting ------\n\ntext: ", m)
 
 plain_numbers = letter_to_number(n, block_size, m)
-print("\nPlain numbers: ", plain_numbers)
+print("\nplain numbers: ", plain_numbers)
 
-encrypted_numbers = AFFINE_encrypt(N, a, b, plain_numbers)
-print("\nEncrypted numbers: ", encrypted_numbers)
+encrypted_numbers = affine_encrypt(n, a, b, plain_numbers)
+print("\nencrypted numbers: ", encrypted_numbers)
 
 encrypted_message = number_to_letter(n, block_size, encrypted_numbers)
-print("\nEncrypted message: ", encrypted_message)
+print("\nencrypted message: ", encrypted_message)
 
 
-# Decryption process
+# decryption process
 
-encrypted_message = "QAOOYQQEVHEQV"
+encrypted_message = "qaooyqqevheqv"
 
-print("\n\n------ Decrypting ------\nEncrypted message: ", encrypted_message)
+print("\n\n------ decrypting ------\nencrypted message: ", encrypted_message)
 
 test_numbers = letter_to_number(n, block_size, encrypted_message)
-print(f"\n## Test numbers ##\nThese should be the numbers of the encrypted message: {test_numbers}")
+print(f"\n## test numbers ##\nthese should be the numbers of the encrypted message: {test_numbers}")
 
-a_inverse = inverse_a(a, N)
-print(f"\nOriginal a: {a} and its inverse {a_inverse}")
+a_inverse = inverse_a(a, n)
+print(f"\noriginal a: {a} and its inverse {a_inverse}")
 
-decrypted_numbers = AFFINE_decrypt(N, a_inverse, b, test_numbers)
-print("\nDecrypted numbers: ", decrypted_numbers)
+decrypted_numbers = affine_decrypt(n, a_inverse, b, test_numbers)
+print("\ndecrypted numbers: ", decrypted_numbers)
 
 decrypted_text = number_to_letter(n, block_size, decrypted_numbers)
-print("\nDecrypted text: ", decrypted_text)
+print("\ndecrypted text: ", decrypted_text)
